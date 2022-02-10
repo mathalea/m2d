e02bd72c142ccb570d5c658f8fa6630279e27a6c
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2022-02-10 12:03:46 +0100

    fix eslint pour ts et js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
+// cf https://dev.to/itmayziii/typescript-eslint-and-standardjs-5hmd (un peu vieux, mais donne une base)
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
+  // root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [{
+    files: ['*.ts'],
     extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended', // Out of the box Typescript rules
+      'standard' // Out of the box StandardJS rules
     ],
-  };
+    parserOptions: {
+      // cf https://github.com/typescript-eslint/typescript-eslint/tree/main/packages/parser#parseroptionsproject
+      project: './tsconfig.json' // Required to have rules that rely on Types.
+      //   'tsconfigRootDir': './'
+    }
+  }, {
+    files: ['*.js'],
+    extends: [
+      'standard' // Out of the box StandardJS rules
+    ]
+  }]
+}
