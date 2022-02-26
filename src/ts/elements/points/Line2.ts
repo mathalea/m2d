f46708252dda746ab34b3a9d179afe1874b5bb0b
Rémi Angot <remi.angot@gmail.com>
2022-02-26 09:28:59 +0100

    Line2 (Il faut que A soit dans la figure)

---
+++ src/ts/elements/points/Line2.ts
@@ -0,0 +1,70 @@
+import { Element2D } from '../Element2D'
+import { Point } from './Point'
+
+export class Line2 extends Element2D {
+    A: Point
+    B: Point
+    constructor (A: Point, B: Point) {
+      super()
+      this.parentFigure = A.parentFigure
+      this.A = A
+      this.B = B
+      const [xOut, yOut, xOut2, yOut2] = getCoordsOut(A, B)
+      const x1Svg = this.parentFigure.xToSx(xOut2)
+      const x2Svg = this.parentFigure.xToSx(xOut)
+      const y1Svg = this.parentFigure.yToSy(yOut2)
+      const y2Svg = this.parentFigure.yToSy(yOut)
+
+      this.g = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+      this.g.setAttribute('x1', `${x1Svg}`)
+      this.g.setAttribute('y1', `${y1Svg}`)
+      this.g.setAttribute('x2', `${x2Svg}`)
+      this.g.setAttribute('y2', `${y2Svg}`)
+
+      // if (!temp)
+      this.parentFigure.svg.appendChild(this.g)
+      this.color = 'blue'
+      A.addDependency(this)
+      B.addDependency(this)
+    }
+
+    update () {
+      const [xOut, yOut, xOut2, yOut2] = getCoordsOut(this.A, this.B)
+      const x1Svg = this.parentFigure.xToSx(xOut2)
+      const x2Svg = this.parentFigure.xToSx(xOut)
+      const y1Svg = this.parentFigure.yToSy(yOut2)
+      const y2Svg = this.parentFigure.yToSy(yOut)
+      this.g.setAttribute('x1', `${x1Svg}`)
+      this.g.setAttribute('y1', `${y1Svg}`)
+      this.g.setAttribute('x2', `${x2Svg}`)
+      this.g.setAttribute('y2', `${y2Svg}`)
+    }
+}
+
+function getCoordsOut (A: Point, B: Point) {
+  const parentFigure = A.parentFigure
+  let pente = Infinity
+  if (B.y !== A.y) {
+    pente = (B.y - A.y) / (B.x - A.x)
+  }
+  if (pente === Infinity) return [A.x, parentFigure.yMax, A.x, parentFigure.yMin]
+  if (pente === 0) return [parentFigure.xMin, A.y, parentFigure.xMax, A.y]
+  let xOut: number, yOut: number
+  let n = 0
+  while (true) {
+    xOut = A.x + n
+    yOut = A.y + n * pente
+    n++
+    if (xOut > parentFigure.xMax || yOut > parentFigure.yMax || yOut < parentFigure.yMin) break
+  }
+  let xOut2: number, yOut2: number
+  n = 0
+  while (true) {
+    xOut2 = A.x + n
+    yOut2 = A.y + n * pente
+    n--
+    if (xOut2 < parentFigure.xMin || yOut2 > parentFigure.yMax || yOut2 < parentFigure.yMin) break
+  }
+  console.log(xOut, yOut, xOut2, yOut2)
+  return [xOut, yOut, xOut2, yOut2]
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,17 +1,6 @@
 
 import { Figure } from './Figure'
-import { Ray } from './elements/lines/Ray'
-import { Bissectrice } from './elements/lines/Bissectrice'
-import { Angle } from './elements/measures/Angle'
-import { DisplayMeasure } from './elements/texts/DisplayMeasure'
-import { CalculDynamic } from './elements/measures/CalculDynamic'
-import { PointByRotation } from './elements/points/PointByRotation'
-import { Vector } from './elements/others/Vector'
-import { PointByTranslationVector } from './elements/points/PointByTranslationVector'
-import { Cursor } from './elements/others/Cursor'
-import { VectorByPoints } from './elements/others/VectorByPoints'
-import { PointByHomothetie } from './elements/points/PointByHomothetie'
-import { Polygon } from './elements/lines/Polygon'
+import { Line2 } from './elements/points/Line2'
 
 /**
  * Script qui permet de tester M2D
@@ -39,42 +28,10 @@ figure.svg.style.margin = 'auto'
 figure.svg.style.display = 'block'
 figure.svg.style.border = 'solid'
 
-console.log(figure)
-
 // On créé des points à partir de leur coordonnées
 
 const A = figure.point(0, 2, { label: 'A' })
 const B = figure.point(4, -2, { label: 'B' })
-const C = figure.point(7, 3, { label: 'C' })
-const D = figure.point(-1, 2, { label: 'D' })
-const c = new Cursor(figure, -5, 5, { min: -2, max: 2, step: 0.1, length: 3, value: 0 })
-const v = new VectorByPoints(C, A)
-const M = new PointByTranslationVector(B, v, { label: 'M' })
-const P = new Polygon(A, C, B, M)
-P.thickness = 2
-P.color = 'blue'
-P.fill = 'yellow'
-P.opacity = 1
-
-const N = new PointByHomothetie(A, D, c.algebraic, { label: '' })
-const U = new PointByHomothetie(C, D, c.algebraic, { label: '' })
-const V = new PointByHomothetie(M, D, c.algebraic, { label: '' })
-const W = new PointByHomothetie(B, D, c.algebraic, { label: '' })
-const Q = new Polygon(N, V, W, U)
-Q.thickness = 1
-Q.color = 'red'
-Q.fill = 'orange'
-Q.opacity = 0.5
-
-const DB = new Ray(D, B)
-DB.dashed = true
-const DC = new Ray(D, C)
-DC.dashed = true
-const DA = new Ray(D, A)
-DA.dashed = true
-const DM = new Ray(D, M)
-DM.dashed = true
-DA.opacity = 0.3
-DM.opacity = 0.3
-DB.opacity = 0.3
-DC.opacity = 0.3
+const L = new Line2(A, B)
+L.color = 'green'
+L.thickness = 2
