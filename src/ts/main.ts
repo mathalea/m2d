4f6779b3e2bd6c07ea53c99618f491b70e5aa7b5
Jean-Claude Lhote <lhotejc@gmail.com>
2022-03-12 21:06:57 +0100

    feat: RandomNumber

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,6 +9,10 @@ import { Point } from './elements/points/Point'
 import { RandomInteger } from './elements/measures/randomInteger'
 import { Vector } from './elements/others/Vector'
 import { PointByTranslationVector } from './elements/points/PointByTranslationVector'
+import { Arc } from './elements/lines/Arc'
+import { ArcBy3Points } from './elements/lines/ArcBy3PointsAndRadius'
+import { RandomNumber } from './elements/measures/randomNumber'
+import { Line } from './elements/lines/Line'
 /*
  * Created by Angot Rémi and Lhote Jean-Claude on 15/02/2022.
  *
@@ -43,12 +47,16 @@ figure.svg.style.border = 'solid'
 // const c2 = new Cursor(figure, -3, 5, { min: -180, max: 180, length: 4, step: 2, value: 0 })
 // const c3 = new Cursor(figure, -3, 4, { min: 0, max: 2, length: 4, step: 0.1, value: 0 })
 // const c4 = new Cursor(figure, -3, 3, { min: -180, max: 180, length: 4, step: 2, value: 0 })
-const c5 = new Cursor(figure, -3, 2, { min: -10, max: 10, length: 4, step: 1, value: 0 })
-const c6 = new Cursor(figure, -3, 1, { min: -10, max: 10, length: 4, step: 1, value: 0 })
-// const x = new RandomInteger(figure, -5, 5)
+const c5 = new Cursor(figure, -3, 2, { min: -90, max: 90, length: 4, step: 1, value: 0 })
+const c6 = new Cursor(figure, -3, 1, { min: 0, max: 4, length: 4, step: 0.4, value: 0 })
+const x = new RandomNumber(figure, 0, 9)
+c6.addChild(x)
 // const y = new RandomInteger(figure, -5, 5)
 
 const A = figure.point(-4, 0, { label: 'A' })
+const B = figure.point(5, 0, { label: 'B' })
+const s = new Line(A, B, { temp: true })
+const M = new PointOnLineAtD(s, x)
 // const M = new PointByHomothetie(B, A, c.algebraic, { label: 'M' })
 // const N = new PointByRotation(B, A, c2.algebraic, { label: 'N' })
 // const P = new PointBySimilitude(B, A, c3.algebraic, c4.algebraic, { label: 'P' })
@@ -57,7 +65,7 @@ const A = figure.point(-4, 0, { label: 'A' })
 // Q.addChild(x)
 // Q.addChild(y)
 // const R = new Point(figure, x, y, { label: 'R' })
-const V = new Vector(figure, c5.algebraic, c6.algebraic)
-const B = new PointByTranslationVector(A, V)
-const s = new Segment(A, B)
-s.style = '->'
+// const V = new Vector(figure, c5.algebraic, c6.algebraic)
+// const B = new PointByTranslationVector(A, V)
+// const s = new Segment(A, B)
+// s.style = '->'
